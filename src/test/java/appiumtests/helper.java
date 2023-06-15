2b8ee6838d66b0334cea1f7140e087ae146e6955
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-06-15 11:17:06 -0700

    small movement from entire screen height

---
+++ src/test/java/appiumtests/helper.java
@@ -0,0 +1,28 @@
+package appiumtests;
+
+import io.appium.java_client.PerformsTouchActions;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.ios.IOSDriver;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+
+import java.awt.*;
+import java.time.Duration;
+
+public class helper {
+
+    public static void scrollDown (IOSDriver driver) {
+        Dimension dimension = driver.manage().window().getSize();
+        int scrollStart = (int) (dimension.getHeight() * 0.5);
+        int scrollEnd = (int) (dimension.getHeight() * 0.8);
+
+        new TouchAction((PerformsTouchActions) driver)
+                .press(PointOption.point(0,scrollStart))
+                .waitAction(WaitOptions.waitOptions(Duration.ofSeconds(2)))
+                .moveTo(PointOption.point(0,scrollEnd))
+                .release().perform();
+    }
+}
