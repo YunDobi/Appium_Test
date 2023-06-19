fce986dd029d77641b415714248aa99239487b9d
Yunchoel Lee <yun@Yunchoels-MacBook-Air.local>
2023-06-19 00:13:43 -0700

    adding read me and valiables

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Appium Finding My Birthday Test #
+Used Appium to practice with IOS Calendar app. When you change the data of birth date,
+it will scroll up automatically. This is my first project that I 
+used Appium, and XCUITest driver with IOS, so I gain the knowledge about the testing methods more.
+
+### How to work ###
+open the FindingDateTest.java, and change the date of the birthday.
+Also have to change udid, platformVersion of the simulator or real device.
+
--- src/test/java/appiumtests/FindingDateTest.java
+++ src/test/java/appiumtests/FindingDateTest.java
@@ -1,37 +1,19 @@
 package appiumtests;
 
 import io.appium.java_client.AppiumBy;
-import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.MobileBy;
-import io.appium.java_client.MobileCommand;
 import io.appium.java_client.ios.IOSDriver;
 import io.appium.java_client.ios.options.XCUITestOptions;
 import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.remote.DesiredCapabilities;
-import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.time.LocalDate;
 import java.time.Month;
-import java.util.HashMap;
 
-public class calculatorTest {
+public class FindingDateTest {
     public static boolean finding = false;
 
-    public static void main(String[] args) {
-        try {
-            openCalculator(args);
-        } catch (Exception exp) {
-//            System.out.println(exp.getCause());
-            System.out.println(exp.getMessage());
-            exp.printStackTrace();
-        }
-    }
-
-    @Test
-    public static void openCalculator(String[] args) throws Exception {
+    public static void main(String[] args) throws MalformedURLException, InterruptedException {
         LocalDate currentDate = LocalDate.now();
         Month currentMonth =  currentDate.getMonth();
         String currentMonthinString = String.valueOf(currentMonth).substring(0,1) + String.valueOf(currentMonth).substring(1).toLowerCase();
@@ -39,20 +21,15 @@ public class calculatorTest {
         String month = "December";
         String days = "16";
 
-
         XCUITestOptions options = new XCUITestOptions()
                 .setUdid("1C2B9B65-3BF1-487B-AAA0-E9962A67A60F")
                 .setPlatformVersion("16.2");
 
         IOSDriver driver = new IOSDriver(
-        new URL("http://127.0.0.1:4723"), options
+                new URL("http://127.0.0.1:4723"), options
         );
-
-
-
-
         driver.findElement(By.xpath("(//XCUIElementTypeIcon[@name=\"Calendar\"])[2]")).click();
-                Thread.sleep(2000);
+        Thread.sleep(2000);
         driver.findElement(By.xpath("//XCUIElementTypeStaticText[@name=\"Today\"]")).click();
 
 
@@ -77,28 +54,18 @@ public class calculatorTest {
             }
             driver.findElement(AppiumBy.accessibilityId(month + year)).click();
             driver.findElement(AppiumBy.accessibilityId("Tuesday, December 16")).click();
+            Thread.sleep(2000);
+            driver.quit();
+            System.out.println("Done");
         }
-        System.out.println("Started");
 
-
-//        HashMap<String, String> scrollObject = new HashMap<String, String>();
-//        scrollObject.put("direction", "up");
-//        scrollObject.put("value", "1997");
-//        System.out.println(scrollObject);
-//
-//        driver.executeScript("mobile:scroll", scrollObject);
-//        driver.findElement(AppiumBy.accessibilityId("1997"));
-
-
-        //Find Developer from Setting
-//        driver.findElement(AppiumBy.accessibilityId("Settings")).click();
-//        Thread.sleep(5000);
-//        HashMap<String, String> scrollObject = new HashMap<String, String>();
-//        scrollObject.put("direction", "down");
-//        scrollObject.put("value", "Developer");
-//
-//        driver.executeScript("mobile:scroll", scrollObject);
-//        driver.findElement(AppiumBy.accessibilityId("DEVELOPER_SETTINGS")).click();
+//        try {
+//            openCalculator(args);
+//        } catch (Exception exp) {
+////            System.out.println(exp.getCause());
+//            System.out.println(exp.getMessage());
+//            exp.printStackTrace();
+//        }
 
     }
 }
